43d9730abf07d24889513ec6e31285dbe0c6ed93
Viswajith Muraleedharan <viswajithmuralee1@gmail.com>
2024-04-20 23:23:05 +0530

    sixth lesson completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,57 @@
 import RestaurantCard from "./RestaurantCard";
-import resObj from "../utils/mockData";
+import Shimmer from "./Shimmer";
 import { useState } from "react";
+import { useEffect } from "react";
 const Body = () => {
-  let [listOfRestaurants, setListOfRestaurants] = useState(resObj);
-  return (
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredlistOfRestaurants, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.91850&lng=76.25580&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  //Conditional Rendering
+  // if (listOfRestaurants.length == 0) {
+  //   return <Shimmer />;
+  // }
+  return listOfRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            // here the value of input tag is bound to the usestate abd its value is "", when typing the value is not updated in the usestate so it is not reflected in the input tag
+            // to make the input value visible I need to change it in the usestate, so we need to use onchange to override usestate value
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredList);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -23,7 +69,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => {
+        {filteredlistOfRestaurants.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
---
+++ src/components/Shimmer.js
@@ -0,0 +1,12 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer_container">
+      <div className="shimmer_card">Cards</div>
+      <div className="shimmer_card">Cards</div>
+      <div className="shimmer_card">Cards</div>
+      <div className="shimmer_card">Cards</div>
+      <div className="shimmer_card">Cards</div>
+    </div>
+  );
+};
+export default Shimmer;
