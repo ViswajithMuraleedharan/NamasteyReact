84eadba2f63ec9389515dab1f065c8d34b04c5d7
Viswajith Muraleedharan <viswajithmuralee1@gmail.com>
2024-04-20 16:37:28 +0530

    commented code removed

--- App.js
+++ App.js
@@ -1,38 +1,3 @@
-// const heading = React.createElement(
-//   "h1",
-//   {
-// id: "heading",
-//   },
-//   "Hello World from React!"
-// );
-
-/* <div id="parent">
-  <div id ="child">
-    <h1>I'm a h1 tag</h1>
-  </div>
-</div> */
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-// "div",
-// { id: "child" },
-// React.createElement("h1", {}, "I'm a h1 tag")
-//   )
-// );
-
-/* <div id="parent">
-  <div id ="child1">
-    <h1>I'm a h1 tag</h1>
-    <h2>I'm a h2 tag</h2>
-  </div>
-  <div id ="child2">
-    <h1>I'm a h1 tag</h1>
-    <h2>I'm a h2 tag</h2>
-  </div>
-</div> */
-
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I'm a h1 tag"),
@@ -44,8 +9,6 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-// console.log(heading); // this gives a React element which is a js object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(root); // gives a ReactDOM object.
-// root.render(heading); // takes the object and create the h1 tag and place it in render
 root.render(parent);
